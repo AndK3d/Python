b25e72a4876262ff55e5bf813294c02589d8defb
AndK3d <33866012+AndK3d@users.noreply.github.com>
2017-12-14 08:57:03 +0200

    save work

--- entity.py
+++ entity.py
@@ -1,10 +1,11 @@
 import sys
 from PyQt5 import QtCore, QtGui, uic
-from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog,QTableWidget,QTableWidgetItem, QHeaderView,QTreeWidgetItem,QTreeWidget
-
+from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QAbstractItemModel, QFile, QIODevice, QModelIndex, Qt
 from PyQt5.QtQuick import QQuickView
-
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 import xml.etree.ElementTree as ET
 
 
@@ -13,14 +14,14 @@ qtCreatorFile = "entity.ui"  # Enter file here.
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 
-class MyApp(QDialog, Ui_MainWindow,QTreeWidget):
+class MyApp(QDialog, Ui_MainWindow):
     def __init__(self):
         QDialog.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         self.browse_folder_bt1.clicked.connect(self.browse_folder)
-
         self.initTable()
+        self.initTreeView()
 
     def browse_folder(self):
 
@@ -28,20 +29,72 @@ class MyApp(QDialog, Ui_MainWindow,QTreeWidget):
 
         if filename:
             self.CIGITaskConfig_path.setText(str(filename[0]))
-            self.get_xml_entity_data(str(filename[0]))
-            data = self.get_xml_entity_data(str(filename[0]))
 
+            data = self.get_xml_entity_data(str(filename[0]))
             self.fill_table(data)
 
+            data = self.get_xml_entity_data(str(filename[0]))
+            self.fill_tree_view(data)
+
             print("point")
+        return
 
-            for row in data:
-                QTreeWidgetItem().setText(0, row)
+    def fill_tree_view(self, data):
 
-                
+        self.treeview.setModel(self.model)
+        self.treeview.setColumnWidth(0, 150)
+        self.treeview.setAlternatingRowColors(True)
+
+        for en in data:
+            #print (en)
+            self.get_child(en)
+
+        value = ["my_item1", "my_item2", "my_item3"]
+
+        #self.item = QStandardItem("my_item1")
+        #self.model.setItem(0, 2, self.item)
+        '''
+        i = 0
+        for val in value:
+            self.item = QStandardItem(val)
+            self.model.setItem(i, 0, item)
+            self.item.appendRow([QStandardItem("Child C"), None])
+            i = i + 1
+        
+        self.treeview.setModel(model)
+        self.treeview.setColumnWidth(0, 150)
+        self.treeview.setAlternatingRowColors(True)
+        '''
+
+        return
+
+    def add_row(self,parent,text='Empty'):
+
+        item = QStandardItem(text)
+        parent.appendRow([item, None])
+
+        return
+
+    def initTreeView(self):
+        self.model = QStandardItemModel()
+
+        # init data
+        self.itemCIGI = QStandardItem("CIGITaskConfig.xml")
+        self.model.setItem(0, 0, self.itemCIGI)
+        self.itemDB = QStandardItem("DBDefaultConfig.xml")
+        self.model.setItem(1, 0, self.itemDB)
+        self.itemRHost = QStandardItem("renderHost*.conf")
+        self.model.setItem(2, 0, self.itemRHost)
 
 
-            #MyApp.Tree.populate(data)
+        self.treeview.setModel(self.model)
+        self.treeview.setColumnWidth(0, 150)
+        self.treeview.setAlternatingRowColors(True)
+
+        self.item_test = self.model.findItems('CIGITaskConfig.xml')
+
+        self.add_row(self.item_test[0])
+
 
         return
 
@@ -61,9 +114,8 @@ class MyApp(QDialog, Ui_MainWindow,QTreeWidget):
     def get_xml_entity_data(self,xml_filename):
 
         xml_tree = ET.parse(xml_filename)
-        entity_list = xml_tree.iter('Entity')
-
-        return entity_list
+        entity_tree = xml_tree.findall('Entity')
+        return entity_tree
 
     def fill_table(self,data):
 
@@ -75,16 +127,24 @@ class MyApp(QDialog, Ui_MainWindow,QTreeWidget):
             i = i + 1
         return
 
-    def populate(self, data):
-        # populate the tree with QTreeWidgetItem items
-        for row in data:
-            # is attached to the root (parent) widget
-            rowItem = QTreeWidgetItem()
-            rowItem.setText(0, row)
-            for subRow in row:
-                # is attached to the current row (rowItem) widget
-                subRowItem = QTreeWidgetItem(rowItem)
-                subRowItem.setText(0, subRow)
+    def get_child(self, parent):
+
+        #print(parent.tag, parent.attrib)
+
+        #self.prnt = self.model.findItems(parent)
+        #self.itemCIGI = QStandardItem(parent.attrib)
+        #self.add_row(self.itemCIGI)
+
+        for key in parent.attrib:
+            print(key, ' = ', parent.attrib[key])
+
+            self.parent_item = self.model.findItems('CIGITaskConfig.xml')
+            print (self.parent_item)
+            self.add_row(self.parent_item)
+
+        for i in range(0, len(parent)):
+            self.get_child(parent[i])
+
 
 
 if __name__ == "__main__":
--- entity_func.py
+++ entity_func.py
@@ -4,10 +4,20 @@ CIGITaskConfig = 'CIGITaskConfig.xml'
 DBDefaultConfig = 'DBDefaultConfig.xml'
 renderHost = 'renderHost_masterV3_insgraf.conf'
 
+
+def get_child(parent):
+    print (parent.tag, parent.attrib)
+
+    for key in parent.attrib:
+        print(key, ' = ', parent.attrib[key])
+    for i in range(0, len(parent)):
+        get_child(parent[i])
+
+
 def get_xml_entity_list(file):
+    xml_tree = ET.parse(file)
+    xml = xml_tree.findall('Entity')
+    for entity in xml:
+        get_child(entity)
 
-    xml_tree = ET.parse(DBDefaultConfig)
-    for entity in xml_tree.iter('entity'):
-        print(entity.attrib)
-
-get_xml_entity_list('E:/py_dev/entity_editor/DBDefaultConfig.xml')
+get_xml_entity_list(CIGITaskConfig)
